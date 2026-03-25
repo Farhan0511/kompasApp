a3f033dc12c01f5ee7b8cbcac9cfd7d96b273f62
farhan0511 <programingsukses@gmail.com>
2026-03-25 10:38:23 +0700

    add components navbar and footer

---
+++ resources/views/user/components/footer.blade.php
@@ -0,0 +1,74 @@
+<footer id="footer" class="footer">
+
+    <!-- Newsletter -->
+    <div class="footer-newsletter">
+        <div class="container">
+            <div class="row justify-content-center text-center">
+                <div class="col-lg-6">
+                    <h4>Info Kompas UNBAJA</h4>
+                    <p>Dapatkan update berita kampus terbaru.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer Top -->
+    <div class="container footer-top">
+        <div class="row gy-4">
+
+            <!-- About -->
+            <div class="col-lg-4 col-md-6 footer-about">
+                <a href="{{ url('/') }}" class="d-flex align-items-center">
+                    <span class="sitename">Kompas UNBAJA</span>
+                </a>
+                <div class="footer-contact pt-3">
+                    <p>Universitas Banten Jaya</p>
+                    <p>Banten, Indonesia</p>
+                    <p class="mt-3"><strong>Email:</strong> <span>kompas@unbaja.ac.id</span></p>
+                </div>
+            </div>
+
+            <!-- Links -->
+            <div class="col-lg-2 col-md-3 footer-links">
+                <h4>Menu</h4>
+                <ul>
+                    <li><i class="bi bi-chevron-right"></i> <a href="#hero">Home</a></li>
+                    <li><i class="bi bi-chevron-right"></i> <a href="#about">About</a></li>
+                    <li><i class="bi bi-chevron-right"></i> <a href="#services">Services</a></li>
+                    <li><i class="bi bi-chevron-right"></i> <a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+
+            <!-- Services -->
+            <div class="col-lg-2 col-md-3 footer-links">
+                <h4>Layanan</h4>
+                <ul>
+                    <li> <span>Berita Kampus</span></li>
+                    <li> <span>Publikasi UKM</span></li>
+                    <li> <span>Sewa Alat Musik</span></li>
+                    <li> <span>Info Kegiatan</span></li>
+                </ul>
+            </div>
+
+            <!-- Social -->
+            <div class="col-lg-4 col-md-12">
+                <h4>Ikuti Kami</h4>
+                <p>Ikuti media sosial Kompas UNBAJA untuk informasi terbaru.</p>
+                <div class="social-links d-flex">
+                    <a href="#"><i class="bi bi-instagram"></i></a>
+                    <a href="#"><i class="bi bi-facebook"></i></a>
+                    <a href="#"><i class="bi bi-youtube"></i></a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Copyright -->
+    <div class="container text-center mt-4">
+        <p>
+            © <strong>Kompas UNBAJA</strong> - All Rights Reserved
+        </p>
+    </div>
+
+</footer>
---
+++ resources/views/user/components/navbar.blade.php
@@ -0,0 +1,22 @@
+<header id="header" class="header d-flex align-items-center sticky-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center">
+
+        <a href="index.html" class="logo d-flex align-items-center me-auto">
+            <img src="{{ asset('views/user/img/kompasapp.jpeg') }}" alt="">
+            <h1 class="sitename">KompasApp</h1>
+        </a>
+
+        <nav id="navmenu" class="navmenu">
+            <ul>
+                <li><a href="#hero" class="active">Home</a></li>
+                <li><a href="#services">News</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+
+        <a class="btn-getstarted" href="index.html#about">Get Started</a>
+
+    </div>
+</header>
