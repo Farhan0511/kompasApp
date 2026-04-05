a3bde150d82ac90dcdb1798925a71f6e729e69fb
farhan0511 <programingsukses@gmail.com>
2026-04-05 17:19:36 +0700

    update: sidebar components belum fix

---
+++ resources/views/admin/components/sidebar.blade.php
@@ -0,0 +1,141 @@
+ <!-- Sidebar -->
+ <div class="sidebar" data-background-color="dark">
+     <div class="sidebar-logo">
+         <!-- Logo Header -->
+         <div class="logo-header" data-background-color="dark">
+             <a href="index.html" class="logo">
+                 <h2 class="text-white">Kompas App</h2>
+             </a>
+             <div class="nav-toggle">
+                 <button class="btn btn-toggle toggle-sidebar">
+                     <i class="gg-menu-right"></i>
+                 </button>
+                 <button class="btn btn-toggle sidenav-toggler">
+                     <i class="gg-menu-left"></i>
+                 </button>
+             </div>
+             <button class="topbar-toggler more">
+                 <i class="gg-more-vertical-alt"></i>
+             </button>
+         </div>
+         <!-- End Logo Header -->
+     </div>
+     <div class="sidebar-wrapper scrollbar scrollbar-inner">
+         <div class="sidebar-content">
+             <ul class="nav nav-secondary">
+                 <li class="nav-item active">
+                     <a data-bs-toggle="collapse" href="#dashboard" class="collapsed" aria-expanded="false">
+                         <i class="fas fa-home"></i>
+                         <p>Dashboard</p>
+                     </a>
+                 <li class="nav-item">
+                     <a data-bs-toggle="collapse" href="#base">
+                         <i class="fas fa-layer-group"></i>
+                         <p>Base</p>
+                     </a>
+                 </li>
+                 <li class="nav-item">
+                     <a data-bs-toggle="collapse" href="#sidebarLayouts">
+                         <i class="fas fa-th-list"></i>
+                         <p>Sidebar Layouts</p>
+                         <span class="caret"></span>
+                     </a>
+                     <div class="collapse" id="sidebarLayouts">
+                         <ul class="nav nav-collapse">
+                             <li>
+                                 <a href="sidebar-style-2.html">
+                                     <span class="sub-item">Sidebar Style 2</span>
+                                 </a>
+                             </li>
+                             <li>
+                                 <a href="icon-menu.html">
+                                     <span class="sub-item">Icon Menu</span>
+                                 </a>
+                             </li>
+                         </ul>
+                     </div>
+                 </li>
+                 <li class="nav-item">
+                     <a data-bs-toggle="collapse" href="#forms">
+                         <i class="fas fa-pen-square"></i>
+                         <p>Forms</p>
+                         <span class="caret"></span>
+                     </a>
+                     <div class="collapse" id="forms">
+                         <ul class="nav nav-collapse">
+                             <li>
+                                 <a href="forms/forms.html">
+                                     <span class="sub-item">Basic Form</span>
+                                 </a>
+                             </li>
+                         </ul>
+                     </div>
+                 </li>
+                 <li class="nav-item">
+                     <a data-bs-toggle="collapse" href="#tables">
+                         <i class="fas fa-table"></i>
+                         <p>Tables</p>
+                         <span class="caret"></span>
+                     </a>
+                     <div class="collapse" id="tables">
+                         <ul class="nav nav-collapse">
+                             <li>
+                                 <a href="tables/tables.html">
+                                     <span class="sub-item">Basic Table</span>
+                                 </a>
+                             </li>
+                             <li>
+                                 <a href="tables/datatables.html">
+                                     <span class="sub-item">Datatables</span>
+                                 </a>
+                             </li>
+                         </ul>
+                     </div>
+                 </li>
+                 <li class="nav-item">
+                     <a data-bs-toggle="collapse" href="#maps">
+                         <i class="fas fa-map-marker-alt"></i>
+                         <p>Maps</p>
+                         <span class="caret"></span>
+                     </a>
+                     <div class="collapse" id="maps">
+                         <ul class="nav nav-collapse">
+                             <li>
+                                 <a href="maps/googlemaps.html">
+                                     <span class="sub-item">Google Maps</span>
+                                 </a>
+                             </li>
+                             <li>
+                                 <a href="maps/jsvectormap.html">
+                                     <span class="sub-item">Jsvectormap</span>
+                                 </a>
+                             </li>
+                         </ul>
+                     </div>
+                 </li>
+                 <li class="nav-item">
+                     <a data-bs-toggle="collapse" href="#charts">
+                         <i class="far fa-chart-bar"></i>
+                         <p>Charts</p>
+                         <span class="caret"></span>
+                     </a>
+                     <div class="collapse" id="charts">
+                         <ul class="nav nav-collapse">
+                             <li>
+                                 <a href="charts/charts.html">
+                                     <span class="sub-item">Chart Js</span>
+                                 </a>
+                             </li>
+                             <li>
+                                 <a href="charts/sparkline.html">
+                                     <span class="sub-item">Sparkline</span>
+                                 </a>
+                             </li>
+                         </ul>
+                     </div>
+                 </li>
+             </ul>
+         </div>
+     </div>
+ </div>
+ <!-- End Sidebar -->
