126283d783ebb7a9f0cbb30eacf1704d3c7b9b1d
farhan0511 <programingsukses@gmail.com>
2026-04-26 20:44:40 +0700

    add controller admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,13 @@ namespace App\Http\Controllers;
 class HomeController extends Controller
 {
     public function index() {
-        return view('user.welcome');
+        return view('user.pages.welcome');
+    }
+
+    public function booking() {
+        return view('user.pages.booking');
+    }
+    public function jadwal(){
+        return view('user.pages.jadwal');
     }
 }
